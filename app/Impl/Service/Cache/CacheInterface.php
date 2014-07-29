98378594f458625de2b0fb17135dbe25066822bc
Justin Page <xjustinpagex@gmail.com>
2014-07-29 10:40:24 -0700

    update: cache architecture

---
+++ app/Impl/Service/Cache/CacheInterface.php
@@ -0,0 +1,33 @@
+<?php namespace Impl\Service\Cache;
+
+interface CacheInterface
+{
+	/**
+	 * Retrieve data from cache
+	 *
+	 * @ param string	Cache item key
+	 * @return mixed	PHP data result of cache
+	 */
+	public function get($key);
+
+	/**
+	 * Add data to the cache
+	 *
+	 * @param string	Cache item key
+	 * @param mixed		The data to store
+	 * @param integer	The number of minutes to store the item
+	 * @return mixed	$value variable returned for convinience
+	 */
+	public function put($key, $value, $minutes=null);
+
+	/**
+	 * Test if items exists in cache
+	 * Only returns true if exists && is not expired
+	 *
+	 * @param string	Cache item $key
+	 * @return bool		If cache item exists
+	 */
+	public function has($key);
+}
+
+
